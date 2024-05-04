621d02cd160f7bc98769a9cae9bed10ecec7d9b5
Pine <167698863+TechMgc@users.noreply.github.com>
2024-05-04 18:33:16 +0800

    Update all.sh

--- all.sh
+++ all.sh
@@ -47,18 +47,17 @@ export uuid=""
 export domain=""
 xray(){
 #安装xray
-#bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install -u root
 
 #配置config.json
-echo "是否需要安装Warp？(输入 y 或 n): "
+uuid=$(xray uuid)
+echo -n -e "${blue}是否需要安装Warp？(输入 y 或 n): ${reset_color}"
 read install_warp
 
-# 如果用户输入'y'，则提示输入Warp端口号，并读取输入
 if [ "$install_warp" = "y" ]; then
-    echo "请输入Warp端口号: "
-    read -warpport
+echo -n -e "${blue}请输入Warp端口号: ${reset_color}"
+read warpport
 fi
-uuid=$(xray uuid)
 # 删除config.json文件中的所有内容
 > /usr/local/etc/xray/config.json
 cat << EOF >> /usr/local/etc/xray/config.json
